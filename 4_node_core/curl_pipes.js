ddb9c5c4398dc70dcd071da751a311afcf73bc33
Matt Walters <mattwalters5@gmail.com>
2014-03-31 00:23:02 -0400

    Update curl_pipes.js

--- 4_node_core/curl_pipes.js
+++ 4_node_core/curl_pipes.js
@@ -4,8 +4,10 @@ var url = process.argv[2] || 'http://google.com'
 // http.request allows you to make an http request
 // against a uri: http://nodejs.org/api/http.html#http_http_request_options_callback
 http.request(url, function (res) {
-  // since you don't know *when* data will come back from the server, we set
-  // up an event handler to listen for those chunks of data as they come back
+  // pipe magically controls back pressure between these two streams (stdout and res).
+  // this essentially means that as data is coming in from the response (and thus from
+  // network IO), mechanics inside the streams api are making sure to only feed that
+  // to stdout as quickly as it can take it.
   res.pipe(process.stdout);
 // remember to call .end() this informs the remote server you're done giving it instructions
 }).end();
