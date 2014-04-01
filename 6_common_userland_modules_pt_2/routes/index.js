a2c90dd8658bbaea0dba5e2f5eaea3331641e7ce
Matt Walters <mattwalters5@gmail.com>
2014-04-01 17:09:18 -0400

    in class examples and 3 image resize on module 6

--- 6_common_userland_modules_pt_2/routes/index.js
+++ 6_common_userland_modules_pt_2/routes/index.js
@@ -7,16 +7,26 @@ exports.upload = function (req, res) {
 }
 
 var gm = require('gm')
+	, fs = require('fs')
   , imageMagick = gm.subClass({ imageMagick: true });
+var async = require('async');
+
 
 exports.resize = function (req, res, next) {
-  // the uploaded file can be found as `req.files.image` and the
+	function magick (size, cb) {
+		// the uploaded file can be found as `req.files.image` and the
+		imageMagick(req.files.image.path)
+			.resize(size, size)
+			.stream('png', function (err, stdout, stderr) {
+				if (err) return cb(err);
+				var one = fs.createWriteStream(__dirname + '/../public/images/' + size + '.png');
+				stdout.pipe(one);
+				cb();
+			});
+	}
   // title field as `req.body.title`
   console.log('resizing %s', req.files.image.name);
-  imageMagick(req.files.image.path)
-    .resize(150, 150)
-    .stream('png', function (err, stdout, stderr) {
-      if (err) return cb(err);
-      stdout.pipe(res);  
-    });
+	async.map([150, 250, 500], magick, function (err) {
+		res.render('images');
+	})
 };
