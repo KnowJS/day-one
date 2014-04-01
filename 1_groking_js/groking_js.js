b4934233e63b41b602a9fed56180069edea713e2
Matt Walters <mattwalters5@gmail.com>
2014-03-31 22:38:25 -0400

    converting tabs to spaces on 1st js file

--- 1_groking_js/groking_js.js
+++ 1_groking_js/groking_js.js
@@ -4,35 +4,35 @@
 
 function save (item, cb) {
   // let's pretend we're adding two numbers on 
-	// a remote server somewhere, and it takes a 
-	// second
-	setTimeout(function () {
-		cb(null, { success: true });
-	}, 1000);
+  // a remote server somewhere, and it takes a 
+  // second
+  setTimeout(function () {
+    cb(null, { success: true });
+  }, 1000);
 }
 
 /*
  * How can do we use this function to save some data?
  */
 save(2, function (err, result) {
-	console.log('saved one!', result)
+  console.log('saved one!', result)
 });
 
 /*
  * What if we want to add a bunch of numbers?
  */
 save(2, function (err, result) {
-	if (err) throw err;
-	console.log('saved many, one at a time!')
-	console.log(result);
-	save(3, function (err, result) {
-		if (err) throw err;
-		console.log(result);
-		save(4, function (err, result) {
-			if (err) throw err;
-			console.log(result);
-		});
-	});
+  if (err) throw err;
+  console.log('saved many, one at a time!')
+  console.log(result);
+  save(3, function (err, result) {
+    if (err) throw err;
+    console.log(result);
+    save(4, function (err, result) {
+      if (err) throw err;
+      console.log(result);
+    });
+  });
 });
 
 /*
